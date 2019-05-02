acdacaf53aa55298436837b2cb22e2402490c140
Christopher Beckham <chrispy645@gmail.com>
2019-05-02 15:54:01 -0400

    add scripts

---
+++ exps/mnist32_downstream/mixupfm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NAME=mnist_mixupfm
+cd ..
+python task_launcher.py \
+--dataset=mnist \
+--arch=architectures/arch_acai_kyle.py \
+--save_every=100 \
+--save_images_every=100 \
+--epochs=5000 \
+--resume=auto \
+--n_channels=1 \
+--ngf=0 \
+--ndf=0 \
+--name=${NAME} \
+--batch_size=64 \
+--beta=0.0 \
+--lamb=10.0 \
+--cls=0.0 \
+--save_path=results_mixup \
+--mixer=fm \
+--seed=1 \
+--classify_encoding='32,10' \
+--weight_decay=1e-5 \
+--beta1=0.5 \
+--beta2=0.99 \
+--lr=1e-4
