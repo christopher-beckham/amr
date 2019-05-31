a0ec9a07e39526c1334cbc3a254b28454cf9965b
Christopher Beckham <cjb60@beluga1.int.ets1.calculquebec.ca>
2019-05-31 11:44:50 -0400

    Add SVHN experiments

---
+++ exps/svhn256_downstream/svhn256_baseline.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd ..
+#source activate pytorch-env
+
+NAME=svhn256_baseline
+python task_launcher.py \
+--dataset=svhn \
+--arch=architectures/arch_acai_kyle_256.py \
+--save_every=100 \
+--save_images_every=100 \
+--epochs=2000 \
+--resume=auto \
+--n_channels=3 \
+--ngf=0 \
+--ndf=0 \
+--name=${NAME} \
+--batch_size=64 \
+--beta=0.0 \
+--lamb=10.0 \
+--cls=0.0 \
+--save_path=results_mixup \
+--mixer=mixup \
+--disable_mix \
+--seed=1 \
+--cls_probe=architectures/cls_probes/linear_legacy.py \
+--cls_probe_args="{'n_in': 256, 'n_classes':10}" \
+--weight_decay=1e-5 \
+--beta1=0.5 \
+--beta2=0.99 \
+--lr=1e-4
+#--mode=interp_train \
+#--mode_override='show_real=1,num_interps=20'
