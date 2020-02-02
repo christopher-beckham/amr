ad7303f9412a1888cee43df71a7af415ffb21a28
Christopher Beckham <cjb60@beluga1.int.ets1.calculquebec.ca>
2020-02-02 15:03:26 -0500

    Add experiments from CC cluster

---
+++ exps/cifar10_1024/acaif3_lamb50.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ..
+#source activate pytorch-env
+
+python task_launcher.py run \
+--model=acaif3 \
+--dataset=cifar10 \
+--arch=architectures/arch_acai_kyle_1024.py \
+--save_every=10 \
+--save_images_every=10 \
+--epochs=2500 \
+--resume=auto \
+--n_channels=3 \
+--ngf=0 \
+--ndf=0 \
+--batch_size=64 \
+--beta=0.0 \
+--lamb=50.0 \
+--cls=0.0 \
+--save_path=results_mixup \
+--mixer=mixup \
+--seed=1 \
+--cls_probe=architectures/cls_probes/linear_legacy.py \
+--cls_probe_args="{'n_in':1024, 'n_classes': 10}" \
+--weight_decay=1e-5 \
+--beta1=0.5 \
+--beta2=0.99 \
+--lr=1e-4
